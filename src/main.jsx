becd5e7d25335e14208a5a8ccb634ef9aa53fe08
mike22a <43526753+mike22a@users.noreply.github.com>
2025-07-13 23:13:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'; // Your main App component
+import App from './App.jsx'; // Your main App component
 import './index.css'; // Import global CSS (for Tailwind)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
